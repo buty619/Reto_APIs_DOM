c8945427d2b8b322a110f8c13393794b801067d3
Cristian Buitrago <cristian@Cristians-MacBook-Pro.local>
2023-05-11 22:39:42 -0500

    solve

--- Reto_de_llamada_APIs/api.js
+++ Reto_de_llamada_APIs/api.js
@@ -1,42 +1,71 @@
 // 1. Obtener información de un Pokemón
 
-function obtenerPokemon(id) {
-  return {};
+async function obtenerPokemon(id) {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data = await APIResponse.json();
+  return {
+    id: data.id,
+    nombre: data.name,
+    tipos: data.types,
+    imagen: data.sprites.front_default,
+  };
 }
 
 obtenerPokemon(1);
 
 // 2. Obtener información de los primeros 20 Pokemón
 
-function obtenerTodosPokemones() {
-  return [];
+async function obtenerTodosPokemones() {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`);
+  const data = await APIResponse.json();
+  return data.results;
 }
 
 obtenerTodosPokemones();
 
 // ### 3. Obtener información del tipo de Pokemón
 
-function obtenerTipo(type) {
-  return [];
+async function obtenerTipo(type) {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/type/${type}/`);
+  const data = await APIResponse.json();
+  return data.pokemon.map((data) => data.pokemon);
 }
 
 obtenerTipo("dragon");
 
 // ### 4. Obtener información de la habilidad Pokemón
 
-function obtenerHabilidadPokemon(idHabilidad) {
-  return {};
+async function obtenerHabilidadPokemon(idHabilidad) {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/ability/${idHabilidad}/`
+  );
+  const data = await APIResponse.json();
+  return {
+    nombre: data.name,
+    id: data.id,
+    pokemones: data.pokemon.map((data) => data.pokemon.name),
+  };
 }
 
 obtenerHabilidadPokemon("1");
 
 // ## 5. Obtener información de la region de un Pokemón
 
-function obtenerRegionPokemon(id) {
-  return {};
+async function obtenerRegionPokemon(id) {
+  const APIResponse1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const pokemon = await APIResponse1.json();
+  const region = pokemon.location_area_encounters;
+  const APIResponse2 = await fetch(region);
+  const data = await APIResponse2.json();
+  return {
+    id: pokemon.id,
+    nombre: pokemon.name,
+    imagen: pokemon.sprites.front_default,
+    region: data.map((region) => region.location_area.name),
+  };
 }
 
-obtenerRegionPokemon(dataBase);
+obtenerRegionPokemon("1");
 
 // aca se exportan todas las funciones para realizar el testing sobre tu solución
 export {
--- Reto_de_manipulacion_DOM/js/script.js
+++ Reto_de_manipulacion_DOM/js/script.js
@@ -1,34 +1,38 @@
-// 1. definir los selectores
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_image");
 
-// const pokemonName = ;
-// const pokemonNumber = ;
+const form = document.querySelector(".form");
+const input = document.querySelector(".input_search");
 
-// const pokemonImage = ;
-// const form = ;
-// const input = ;
-
-// const buttonPrev = ;
-// const buttonNext = ;
+const buttonPrev = document.querySelector(".anterior");
+const buttonNext = document.querySelector(".proximo");
 
 let start = 1;
 
-// ### 2. crear la función que consuma de la API de pokemon
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-const fetchPokemon = async (pokemonID) => {
-  return pokemonData;
+  if ((await APIResponse).status == 200) {
+    const data = (await APIResponse).json();
+    return data;
+  }
+  return {};
 };
 
-// ### 3. crear la función renderPokemon
-
-const renderPokemon = async (pokemonID) => {
+const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = "cargando...";
   pokemonNumber.innerHTML = "";
 
-  const data = await fetchPokemon(pokemonID);
+  const data = await fetchPokemon(pokemon);
 
-  if (data) {
-    // agregar la información obtenida en los campos pokemonName, pokemonNumber y pokemonImage
-
+  if (data.name) {
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
     input.value = "";
     start = data.id;
   } else {
@@ -38,12 +42,21 @@ const renderPokemon = async (pokemonID) => {
   }
 };
 
-// ### 4. agregar event listeners
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
 
-// form.addEventListener... ;
+buttonPrev.addEventListener("click", () => {
+  if (start > 1) {
+    start -= 1;
+    renderPokemon(start);
+  }
+});
 
-// buttonPrev.addEventListener... ;
-
-// buttonNext.addEventListener ...;
+buttonNext.addEventListener("click", () => {
+  start += 1;
+  renderPokemon(start);
+});
 
 renderPokemon(start);
